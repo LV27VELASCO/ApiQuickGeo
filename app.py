a207bb5f07afdb8e9d5c4333e5dd24c1c58cfd68
LV27VELASCO <luiselsw27@gmail.com>
2025-01-25 11:46:27 -0500

    agregando nuevo endpoint

--- app.py
+++ app.py
@@ -1,25 +1,29 @@
+import os
+
 import folium
-from opencage.geocoder import OpenCageGeocode
-from flask import Flask, request, jsonify
-from pydantic import BaseModel, ValidationError
 import phonenumbers
-from phonenumbers import geocoder, carrier, timezone
+from dotenv import load_dotenv
+from flask import Flask, jsonify, request
 from flask_cors import CORS
+from phonenumbers import carrier, geocoder
+from pydantic import ValidationError
+from vonage import Auth, Sms, Vonage
+from vonage_sms import SmsMessage, SmsResponse
+
+from models import PhoneNumberInput, PhoneNumberOut, SendSmsInput, SendSmsOut
+
+load_dotenv()
+
 
 app = Flask(__name__)
 # Habilitar CORS para todas las rutas
 CORS(app, resources={ r"/*": {"origins": ["http://localhost:4200","https://fullgeoclone.netlify.app"]}})
 
-# Modelo de entrada
-class PhoneNumberInput(BaseModel):
-    code:str
-    phone_number: str
-    code_lang: str
 
-# Modelo de salida
-class PhoneNumberInfo(BaseModel):
-    country: str
-    operator: str
+API_KEY = os.environ.get("API_KEY")
+API_SECRET = os.environ.get("API_SECRET")
+BRAND_NAME = os.environ.get("BRAND_NAME")
+client = Vonage(Auth(api_key=API_KEY, api_secret=API_SECRET))
 
 @app.route('/phone-info', methods=['POST'])
 def get_phone_info():
@@ -38,7 +42,7 @@ def get_phone_info():
         operator = carrier.name_for_number(parsed_number, code_lang)  # Operador en inglés
 
         # Modelo de salida
-        response = PhoneNumberInfo(country=country, operator=operator)
+        response = PhoneNumberOut(country=country, operator=operator)
         return jsonify(response.dict()), 200
 
     except ValidationError as e:
@@ -47,6 +51,45 @@ def get_phone_info():
         return jsonify({"error": f"Invalid phone number: {str(e)}"}), 400
     except Exception as e:
         return jsonify({"error": str(e)}), 500
+    
 
+@app.route('/send-sms', methods=['POST'])
+def send_sms():
+    try:
+        # Validamos los datos de entrada con Pydantic
+        data = SendSmsInput.parse_obj(request.json)
+        code = data.code
+        phone_number = data.phone_number
+
+        # Procesamos el número telefónico con phonenumbers
+        parsed_number = phonenumbers.parse(f"{code}{phone_number}")
+        
+        message = SmsMessage(
+            to=parsed_number,
+            from_=BRAND_NAME,
+            text="Localiza tu telefono aquí: .",
+            )
+        
+        response: SmsResponse = client.sms.send(message)
+
+        if hasattr(response, "messages") and len(response.messages) > 0:
+            # Acceder al primer mensaje en la lista
+            message = response.messages[0]
+            if message.status == "0":
+                # Modelo de salida
+                response = SendSmsOut(status=True, description="Sms enviado correctamente")
+                return jsonify(response.dict()), 200
+            else:
+                response = SendSmsOut(status=False, description="No se pudo enviar correctamente el mensaje")
+                return jsonify(response.dict()), 400
+        else:
+            response = SendSmsOut(status=False, description="Ocurrió un error en el envio del mensaje")
+            return jsonify(response.dict()), 500
+        
+    except ValidationError as e:
+        return jsonify({"error": str(e)}), 400
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+    
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ models.py
@@ -0,0 +1,21 @@
+from pydantic import BaseModel
+# Modelo de entrada phone-info
+class PhoneNumberInput(BaseModel):
+    code:str
+    phone_number: str
+    code_lang: str
+
+# Modelo de salida phone-info
+class PhoneNumberOut(BaseModel):
+    country: str
+    operator: str
+
+# Modelo de entrada send-sms
+class SendSmsInput(BaseModel):
+    code:str
+    phone_number: str
+
+# Modelo de salida send-sms
+class SendSmsOut(BaseModel):
+    status:bool
+    description: str
